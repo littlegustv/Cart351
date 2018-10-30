706461a454ac419f5b74738deab7ddbf07727ee5
Benny Heller <echo29x@gmail.com>
2018-10-29 21:57:22 -0400

    week seven

--- exercises/seven/index.php
+++ exercises/seven/index.php
@@ -1,12 +1,29 @@
 <?php
-  session_start();
-  $_SESSION['Name'] = $_POST['Name'];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  $data = new stdClass();
+  $data->name = $_POST['Name'];
+  $data->multiselect = $_POST['Multiselect'];
+  $data->color = $_POST['Color'];
+  $data->date = $_POST['Date'];
+
+  $filename = $_FILES['Filename']['name'];
+  move_uploaded_file($_FILES['Filename']['tmp_name'], 'images/' . $filename);
+
+  $data->image = 'images/' . $filename;
+
+  $json = json_encode($data);
+  echo $json;
+  exit;
+
+}
+
  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>HELLO, <?php echo $_SESSION['Name']; ?></title>
+    <title></title>
     <style media="screen">
       * {
         margin: 0;
@@ -17,32 +34,70 @@
         width: 100%;
       }
       body {
-        background: <?php echo $_POST['Color']; ?>;
         color: white;
         text-align: center;
       }
     </style>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function (e) {
+        $("#myForm").submit(function (e) {
+          e.preventDefault();
+          let form = $('#myForm')[0];
+          let data = new FormData(form);
+
+          $.ajax({
+            type: "POST",
+            enctype: "multipart/form-data",
+            url: "index.php",
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (response) {
+              //console.log(response);
+              let data = JSON.parse(response);
+              let container = $('<div>');
+              for (key in data) {
+                if (key == "color") {
+                  $('body').css('background', data[key]);
+                } else if (key == "image") {
+                  let row = $('<img>');
+                  row.attr("src", data[key]);
+                  container.append(row);
+                } else {
+                  let row = $('<p>');
+                  row.text(key + ": " + data[key]);
+                  container.append(row);
+                }
+              }
+              $('body').append(container);
+              console.log(data);
+            },
+            error: function () {
+              console.log("error occurred");
+            }
+          })
+        });
+      });
+    </script>
   </head>
   <body>
-    <form class="" action="" method="post">
+    <form id="myForm" class="" action="" method="post" enctype="multipart/form-data">
       <label for="">
         What is your name?
-        <input type="text" name="Name" value="<?php echo $_POST['Name']; ?>">
+        <input type="text" name="Name" value="">
       </label>
       <select class="" name="Multiselect[]">
         <option value="Robespierre">Robespierre</option>
         <option value="Danton">Danton</option>
         <option value="Desmoulins">Desmoulins</option>
       </select>
-      <input type="color" name="Color" value="<?php echo $_POST['Color']; ?>">
-      <input type="date" name="Date" value="<?php echo $_POST['Date']; ?>">
+      <input type="color" name="Color" value="">
+      <input type="date" name="Date" value="">
+      <input type="file" name="Filename" value="" size=10 required>
       <input type="submit" name="" value="Go!">
     </form>
-    <h1>
-     <?php echo $_POST['Date']; ?>
-    </h1>
-    <p>
-      <?php var_dump($_POST['Multiselect']); ?>
-    </p>
   </body>
 </html>
